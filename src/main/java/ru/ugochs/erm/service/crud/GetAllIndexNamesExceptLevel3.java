be0f48213395845f30e17d7e3fb694f8af6dd144
sl4r1k <sl4r1k@gmail.com>
2022-03-17 01:06:53 +0300

    добавить реализацию чтения из БД всех индексов кроме третьего уровня иерархии

---
+++ src/main/java/ru/ugochs/erm/service/crud/GetAllIndexNamesExceptLevel3.java
@@ -0,0 +1,25 @@
+package ru.ugochs.erm.service.crud;
+
+import ru.ugochs.erm.entity.Index;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GetAllIndexNamesExceptLevel3 extends CrudOperation<Index, List<String>> {
+    public GetAllIndexNamesExceptLevel3(Db db) {
+        super(db);
+    }
+
+    @Override
+    public List<String> perform() {
+        return new TupleCriteria<>(
+            (criteria, query, index) -> query.multiselect(index.get("name")).where(
+                criteria.notEqual(
+                    index.get("level"), 3
+                )
+            ),
+            this.db, Index.class
+        ).query().getResultList().stream()
+            .map(tuple -> tuple.get(0, String.class))
+            .collect(Collectors.toList());
+    }
+}
