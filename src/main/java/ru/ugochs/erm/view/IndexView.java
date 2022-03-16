9fe4241e4915be52effcef60d0a49551161d9e3e
sl4r1k <sl4r1k@gmail.com>
2022-03-17 01:32:13 +0300

    добавить страницу для работы с индексами

---
+++ src/main/java/ru/ugochs/erm/view/IndexView.java
@@ -0,0 +1,49 @@
+package ru.ugochs.erm.view;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.treegrid.TreeGrid;
+import com.vaadin.flow.router.*;
+import ru.ugochs.erm.entity.Index;
+import ru.ugochs.erm.service.FilterIndexesByRelativeName;
+import ru.ugochs.erm.service.crud.*;
+import ru.ugochs.erm.view.component.*;
+import ru.ugochs.erm.view.form.add.AddIndexForm;
+import ru.ugochs.erm.view.form.edit.EditIndexForm;
+
+@PageTitle("Индексы")
+@Route("indexes")
+public class IndexView extends VerticalLayout implements BeforeEnterObserver {
+    private final Db db;
+    private final TreeGrid<Index> indexes;
+
+    public IndexView(Db db) {
+        this.db = db;
+        this.indexes = new StandardTreeGrid<>(
+            Index::getName,
+            clickEvent -> new NavigationByEntity(clickEvent.getItem(), EditIndexForm.class).perform()
+        );
+        this.add(
+            new StandardHorizontalLayout(
+                new H2("Индексы"),
+                new FilterTextField(event ->
+                    this.indexes.setItems(
+                        new FilterIndexesByRelativeName(event.getValue(), this.db).perform(),
+                        Index::getChildren
+                    )
+                ),
+                new Button("Добавить", event -> new Navigation(AddIndexForm.class).perform())
+            ),
+            this.indexes
+        );
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        this.indexes.setItems(
+            new GetAllIndexesByLevel1(this.db).perform(),
+            Index::getChildren
+        );
+    }
+}
