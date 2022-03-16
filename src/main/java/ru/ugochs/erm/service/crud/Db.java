64539ab8d0715a245c831f21ceb4561a4f0890fe
sl4r1k <sl4r1k@gmail.com>
2022-03-16 22:31:41 +0300

    добавить утилитный класс для работы с БД

---
+++ src/main/java/ru/ugochs/erm/service/crud/Db.java
@@ -0,0 +1,22 @@
+package ru.ugochs.erm.service.crud;
+
+import lombok.experimental.Delegate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Service
+public class Db {
+    @Delegate(types = EntityManager.class)
+    @PersistenceContext
+    private EntityManager em;
+
+    @Delegate(types = TransactionTemplate.class)
+    private TransactionTemplate template;
+
+    public Db(PlatformTransactionManager ptm) {
+        this.template = new TransactionTemplate(ptm);
+    }
+}
