6e240f656f4fb46ca15ac0172b3d6107b9a4f597
sl4r1k <sl4r1k@gmail.com>
2022-03-17 01:22:07 +0300

    добавить кастомное текстовое поле

---
+++ src/main/java/ru/ugochs/erm/view/component/BindedTextField.java
@@ -0,0 +1,15 @@
+package ru.ugochs.erm.view.component;
+
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.Setter;
+import com.vaadin.flow.function.ValueProvider;
+import ru.ugochs.erm.entity.AbstractEntity;
+
+public class BindedTextField<T extends AbstractEntity> extends TextField {
+    public BindedTextField(String label, Binder<T> binder,
+                           ValueProvider<T, String> getter, Setter<T, String> setter) {
+        super(label);
+        binder.forField(this).bind(getter, setter);
+    }
+}
