61ecb1faa981852280846bbd5b13c76d8b0464a4
sl4r1k <sl4r1k@gmail.com>
2022-03-29 16:15:40 +0300

    добавить операцию проверки на существующую сущность, игнорируя сущность с тем же id

---
+++ src/main/java/ru/ugochs/erm/service/crud/ExistsByAttributeIgnoringItself.java
@@ -0,0 +1,27 @@
+package ru.ugochs.erm.service.crud;
+
+import ru.ugochs.erm.entity.AbstractEntity;
+
+public class ExistsByAttributeIgnoringItself<T extends AbstractEntity> extends ExistsByAttribute<T> {
+    private final Long id;
+
+    public ExistsByAttributeIgnoringItself(String attribute, Object value, Long id, Db db, Class type) {
+        super(attribute, value, db, type);
+        this.id = id;
+    }
+
+    @Override
+    protected Boolean perform() {
+        return this.db.createQuery(
+            String.format(
+                "SELECT CASE WHEN COUNT(x) > 0 THEN TRUE ELSE FALSE END FROM %s AS x WHERE x.%s = :value AND x.id <> :id",
+                this.type.getSimpleName(),
+                this.attribute,
+                this.id
+            ),
+            Boolean.class
+        ).setParameter("value", this.value)
+            .setParameter("id", this.id)
+            .getSingleResult();
+    }
+}
