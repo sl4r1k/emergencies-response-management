74e0056963bbdc8c6864ff1508bd30b5b7b779f9
sl4r1k <sl4r1k@gmail.com>
2022-03-29 16:16:26 +0300

    добавить проверку на уже существующую сущность

--- src/main/java/ru/ugochs/erm/service/crud/EditIndex.java
+++ src/main/java/ru/ugochs/erm/service/crud/EditIndex.java
@@ -1,6 +1,7 @@
 package ru.ugochs.erm.service.crud;
 
 import ru.ugochs.erm.entity.Index;
+import javax.persistence.EntityExistsException;
 
 public class EditIndex extends Edit<Index> {
     public EditIndex(Index index, Db db) {
@@ -9,6 +10,14 @@ public class EditIndex extends Edit<Index> {
 
     @Override
     public Index perform() {
+        if (new ExistsByAttributeIgnoringItself<>(
+            "name", this.entity.getName(), this.entity.getId(),
+            this.db, Index.class
+        ).perform()) {
+            throw new EntityExistsException(
+                "Индекс с таким названием уже существует"
+            );
+        }
         return super.perform();
     }
 }
