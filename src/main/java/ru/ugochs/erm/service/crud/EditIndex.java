e7f60d041d31f843b820a32c2d66103ecea8b9a6
sl4r1k <sl4r1k@gmail.com>
2022-03-29 19:45:26 +0300

    добавить наследование операции редактирования индекса

--- src/main/java/ru/ugochs/erm/service/crud/EditIndex.java
+++ src/main/java/ru/ugochs/erm/service/crud/EditIndex.java
@@ -1,23 +1,16 @@
 package ru.ugochs.erm.service.crud;
 
 import ru.ugochs.erm.entity.Index;
-import javax.persistence.EntityExistsException;
 
-public class EditIndex extends Edit<Index> {
+public class EditIndex extends EditIfNotExists<Index> {
     public EditIndex(Index index, Db db) {
-        super(index, db);
-    }
-
-    @Override
-    public Index perform() {
-        if (new ExistsByAttributeIgnoringItself<>(
-            "name", this.entity.getName(), this.entity.getId(),
-            this.db, Index.class
-        ).perform()) {
-            throw new EntityExistsException(
-                "Индекс с таким названием уже существует"
-            );
-        }
-        return super.perform();
+        super(
+            index,
+            "name",
+            index.getName(),
+            index.getId(),
+            "Индекс с таким названием уже существует",
+            db
+        );
     }
 }
