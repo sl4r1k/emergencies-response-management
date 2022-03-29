f77ff3c70a33e37c2ddf8892e358658815772b09
sl4r1k <sl4r1k@gmail.com>
2022-03-29 16:38:02 +0300

    добавить сортировку для иерархического списка

--- src/main/java/ru/ugochs/erm/view/component/StandardTreeGrid.java
+++ src/main/java/ru/ugochs/erm/view/component/StandardTreeGrid.java
@@ -1,8 +1,7 @@
 package ru.ugochs.erm.view.component;
 
 import com.vaadin.flow.component.ComponentEventListener;
-import com.vaadin.flow.component.grid.GridVariant;
-import com.vaadin.flow.component.grid.ItemDoubleClickEvent;
+import com.vaadin.flow.component.grid.*;
 import com.vaadin.flow.component.treegrid.TreeGrid;
 import com.vaadin.flow.function.ValueProvider;
 import ru.ugochs.erm.entity.AbstractEntity;
@@ -11,6 +10,7 @@ public class StandardTreeGrid<T extends AbstractEntity> extends TreeGrid<T> {
     public StandardTreeGrid(ValueProvider<T, ?> value, ComponentEventListener<ItemDoubleClickEvent<T>> clickEvent) {
         this.addHierarchyColumn(value);
         this.addItemDoubleClickListener(clickEvent);
+        this.sort(GridSortOrder.asc(this.getColumns().get(0)).build());
         this.addThemeVariants(GridVariant.LUMO_NO_BORDER);
         this.getStyle().set("font-size", "140%");
     }
