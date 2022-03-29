2b9fb662b3e7827734f7bb9e1bfb8f8400540980
sl4r1k <sl4r1k@gmail.com>
2022-03-29 19:50:15 +0300

    добавить форму редактирования заявителей

---
+++ src/main/java/ru/ugochs/erm/view/form/edit/EditReporterForm.java
@@ -0,0 +1,47 @@
+package ru.ugochs.erm.view.form.edit;
+
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.*;
+import ru.ugochs.erm.entity.Reporter;
+import ru.ugochs.erm.service.crud.*;
+import ru.ugochs.erm.view.IdFromRoute;
+import ru.ugochs.erm.view.ReporterView;
+import ru.ugochs.erm.view.component.*;
+import ru.ugochs.erm.view.form.ReporterForm;
+
+@Route("reporters/:id/edit")
+public class EditReporterForm extends ReporterForm implements BeforeEnterObserver {
+    public EditReporterForm(Db db) {
+        super(db);
+        this.add(
+            new HorizontalLayout(
+                new EditButton<>(
+                    reporter -> new EditReporter(reporter, this.db),
+                    this.binder,
+                    ReporterView.class
+                ),
+                new RemoveButton<>(
+                    reporter -> new Remove<>(reporter, this.db),
+                    this.binder,
+                    ReporterView.class
+                ),
+                new CancelButton(ReporterView.class)
+            )
+        );
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        new GetById<>(
+            new IdFromRoute(event.getRouteParameters()).value(),
+            this.db,
+            Reporter.class
+        ).perform().ifPresentOrElse(
+            this.binder::setBean,
+            () -> event.rerouteToError(
+                NotFoundException.class,
+                "Нет такого индекса"
+            )
+        );
+    }
+}
