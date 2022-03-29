a6b81a37c9b32a4e88fbed15ebac2bc804dd859a
sl4r1k <sl4r1k@gmail.com>
2022-03-29 19:43:03 +0300

    добавить проверку на наличие сущности при редактировании

---
+++ src/main/java/ru/ugochs/erm/service/crud/EditIfNotExists.java
@@ -0,0 +1,43 @@
+package ru.ugochs.erm.service.crud;
+
+import ru.ugochs.erm.entity.AbstractEntity;
+import ru.ugochs.erm.entity.Index;
+import javax.persistence.EntityExistsException;
+
+public class EditIfNotExists<T extends AbstractEntity> extends Edit<T> {
+    private final String attribute;
+    private final Object value;
+    private final Long id;
+    private final String message;
+
+    public EditIfNotExists(
+        T entity,
+        String attribute,
+        Object value,
+        Long id,
+        String message,
+        Db db
+    ) {
+        super(entity, db);
+        this.attribute = attribute;
+        this.value = value;
+        this.id = id;
+        this.message = message;
+    }
+
+    @Override
+    public T perform() {
+        if (
+            new ExistsByAttributeIgnoringItself<>(
+                this.attribute,
+                this.value,
+                this.id,
+                this.db,
+                Index.class
+            ).perform()
+        ) {
+            throw new EntityExistsException(this.message);
+        }
+        return super.perform();
+    }
+}
