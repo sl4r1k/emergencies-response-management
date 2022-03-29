29c2da117ad38b0c3e8b992bde5a7a1f4a78afd6
sl4r1k <sl4r1k@gmail.com>
2022-03-29 17:39:40 +0300

    добавить отображение окна ошибки добавления

--- src/main/java/ru/ugochs/erm/view/component/AddButton.java
+++ src/main/java/ru/ugochs/erm/view/component/AddButton.java
@@ -4,8 +4,11 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.data.binder.Binder;
+import org.claspina.confirmdialog.ButtonOption;
+import org.claspina.confirmdialog.ConfirmDialog;
 import ru.ugochs.erm.entity.AbstractEntity;
 import ru.ugochs.erm.service.crud.Add;
+import javax.persistence.EntityExistsException;
 import java.util.function.Function;
 
 public class AddButton<T extends AbstractEntity> extends Button {
@@ -16,7 +19,20 @@ public class AddButton<T extends AbstractEntity> extends Button {
         super(
             "Добавить",
             event -> {
-                operation.apply(binder.getBean()).perform();
+                if (!binder.validate().isOk()) {
+                    return;
+                }
+                try {
+                    operation.apply(binder.getBean()).perform();
+                } catch (EntityExistsException exception) {
+                    ConfirmDialog.createError()
+                        .withCaption("Ошибка добавления")
+                        .withMessage("Такая запись уже существует!")
+                        .withOkButton(
+                            ButtonOption.caption("Понятно"),
+                            ButtonOption.focus()
+                        ).open();
+                }
                 new BackInHistory().perform();
             }
         );
