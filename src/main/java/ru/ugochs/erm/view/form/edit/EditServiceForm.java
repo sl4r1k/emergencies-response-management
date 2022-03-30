aa1c9c5a4897877eb923209a2dd4fb6d2d3d19a8
sl4r1k <sl4r1k@gmail.com>
2022-03-30 11:09:35 +0300

    добавить форму редактирования службы

---
+++ src/main/java/ru/ugochs/erm/view/form/edit/EditServiceForm.java
@@ -0,0 +1,47 @@
+package ru.ugochs.erm.view.form.edit;
+
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.*;
+import ru.ugochs.erm.entity.Service;
+import ru.ugochs.erm.service.crud.*;
+import ru.ugochs.erm.view.IdFromRoute;
+import ru.ugochs.erm.view.ServiceView;
+import ru.ugochs.erm.view.component.*;
+import ru.ugochs.erm.view.form.ServiceForm;
+
+@Route("services/:id/edit")
+public class EditServiceForm extends ServiceForm implements BeforeEnterObserver {
+    public EditServiceForm(Db db) {
+        super(db);
+        this.add(
+            new HorizontalLayout(
+                new EditButton<>(
+                    service -> new EditService(service, this.db),
+                    this.binder,
+                    ServiceView.class
+                ),
+                new RemoveButton<>(
+                    service -> new Remove<>(service, this.db),
+                    this.binder,
+                    ServiceView.class
+                ),
+                new CancelButton(ServiceView.class)
+            )
+        );
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        new GetById<>(
+            new IdFromRoute(event.getRouteParameters()).value(),
+            this.db,
+            Service.class
+        ).perform().ifPresentOrElse(
+            this.binder::setBean,
+            () -> event.rerouteToError(
+                NotFoundException.class,
+                "Нет такого индекса"
+            )
+        );
+    }
+}
