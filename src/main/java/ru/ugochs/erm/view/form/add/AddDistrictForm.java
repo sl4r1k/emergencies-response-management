8c145c817e4e03d24a66ede59ebb9bcef4cddc3b
sl4r1k <sl4r1k@gmail.com>
2022-03-30 11:47:47 +0300

    добавить форму сохранения района

---
+++ src/main/java/ru/ugochs/erm/view/form/add/AddDistrictForm.java
@@ -0,0 +1,29 @@
+package ru.ugochs.erm.view.form.add;
+
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.Route;
+import ru.ugochs.erm.entity.District;
+import ru.ugochs.erm.service.crud.AddDistrict;
+import ru.ugochs.erm.service.crud.Db;
+import ru.ugochs.erm.view.ServiceView;
+import ru.ugochs.erm.view.component.AddButton;
+import ru.ugochs.erm.view.component.CancelButton;
+import ru.ugochs.erm.view.form.DistrictForm;
+
+@Route("districts/add")
+public class AddDistrictForm extends DistrictForm {
+    public AddDistrictForm(Db db) {
+        super(db);
+        this.binder.setBean(new District());
+        this.add(
+            new HorizontalLayout(
+                new AddButton<>(
+                    district -> new AddDistrict(district, this.db),
+                    this.binder,
+                    DistrictView.class
+                ),
+                new CancelButton(DistrictView.class)
+            )
+        );
+    }
+}
