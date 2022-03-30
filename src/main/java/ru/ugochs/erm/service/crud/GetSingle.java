bbb7f1b7f370f608ffd294c6c2ca8f55f4474dd5
sl4r1k <sl4r1k@gmail.com>
2022-03-30 11:12:47 +0300

    добавить логику в операцию чтения по атрибуту

--- src/main/java/ru/ugochs/erm/service/crud/GetSingle.java
+++ src/main/java/ru/ugochs/erm/service/crud/GetSingle.java
@@ -1,10 +1,35 @@
 package ru.ugochs.erm.service.crud;
 
 import ru.ugochs.erm.entity.AbstractEntity;
+import javax.persistence.NoResultException;
 import java.util.Optional;
 
 public abstract class GetSingle<T extends AbstractEntity> extends CrudOperation<T, Optional<T>> {
-    protected GetSingle(Db db) {
+    private final String attribute;
+    private final Object value;
+    private final Class<T> type;
+
+    protected GetSingle(String attribute, Object value, Db db, Class<T> type) {
         super(db);
+        this.attribute = attribute;
+        this.value = value;
+        this.type = type;
+    }
+
+    @Override
+    public Optional<T> perform() {
+        try {
+            return Optional.of(
+                new EntityCriteria<>((criteria, query, entity) -> query.select(entity).where(
+                    criteria.equal(
+                        entity.get(this.attribute), this.value
+                    )
+                ),
+                    this.db, this.type
+                ).query().getSingleResult()
+            );
+        } catch (NoResultException exception) {
+            return Optional.empty();
+        }
     }
 }
