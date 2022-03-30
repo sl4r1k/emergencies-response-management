468c36a5425538cea4c047d215a443b711be697c
sl4r1k <sl4r1k@gmail.com>
2022-03-30 11:51:05 +0300

    добавить форму редактирования района

---
+++ src/main/java/ru/ugochs/erm/view/form/edit/EditDistrictForm.java
@@ -0,0 +1,47 @@
+package ru.ugochs.erm.view.form.edit;
+
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.*;
+import ru.ugochs.erm.entity.District;
+import ru.ugochs.erm.service.crud.*;
+import ru.ugochs.erm.view.DistrictView;
+import ru.ugochs.erm.view.IdFromRoute;
+import ru.ugochs.erm.view.component.*;
+import ru.ugochs.erm.view.form.DistrictForm;
+
+@Route("districts/:id/edit")
+public class EditDistrictForm extends DistrictForm implements BeforeEnterObserver {
+    public EditDistrictForm(Db db) {
+        super(db);
+        this.add(
+            new HorizontalLayout(
+                new EditButton<>(
+                    district -> new EditDistrict(district, this.db),
+                    this.binder,
+                    DistrictView.class
+                ),
+                new RemoveButton<>(
+                    district -> new Remove<>(district, this.db),
+                    this.binder,
+                    DistrictView.class
+                ),
+                new CancelButton(DistrictView.class)
+            )
+        );
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        new GetById<>(
+            new IdFromRoute(event.getRouteParameters()).value(),
+            this.db,
+            District.class
+        ).perform().ifPresentOrElse(
+            this.binder::setBean,
+            () -> event.rerouteToError(
+                NotFoundException.class,
+                "Нет такого индекса"
+            )
+        );
+    }
+}
