ae4e782de6c4ca275e38453e6c048f4e661c98d3
sl4r1k <sl4r1k@gmail.com>
2022-03-30 11:55:50 +0300

    добавить страницу для работы с районами

---
+++ src/main/java/ru/ugochs/erm/view/DistrictView.java
@@ -0,0 +1,50 @@
+package ru.ugochs.erm.view;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.router.*;
+import ru.ugochs.erm.entity.District;
+import ru.ugochs.erm.service.crud.Db;
+import ru.ugochs.erm.service.crud.GetAll;
+import ru.ugochs.erm.view.component.*;
+import ru.ugochs.erm.view.form.add.AddDistrictForm;
+import ru.ugochs.erm.view.form.edit.EditDistrictForm;
+
+@PageTitle("Районы")
+@Route("districts")
+public class DistrictView extends FullSizedVerticalLayout implements BeforeEnterObserver {
+    private final Db db;
+    private final Grid<District> districts;
+
+    public DistrictView(Db db) {
+        this.db = db;
+        this.districts = new StandardGrid<>(
+            District::getName,
+            clickEvent -> new NavigationByEntity(
+                clickEvent.getItem(),
+                EditDistrictForm.class
+            ).perform()
+        );
+        this.add(
+            new StandardHorizontalLayout(
+                new H2("Районы"),
+                new Button(
+                    "Добавить",
+                    event -> new Navigation(AddDistrictForm.class).perform()
+                )
+            ),
+            this.districts
+        );
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        this.districts.setItems(
+            new GetAll<>(
+                this.db,
+                District.class
+            ).perform()
+        );
+    }
+}
