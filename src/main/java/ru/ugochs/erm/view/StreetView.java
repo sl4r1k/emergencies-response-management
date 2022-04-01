9eb6819e223ed1a294bb41b552fc1acb7cc5bdbf
sl4r1k <sl4r1k@gmail.com>
2022-04-01 20:06:09 +0300

    добавить страницу для работы с улицами

---
+++ src/main/java/ru/ugochs/erm/view/StreetView.java
@@ -0,0 +1,60 @@
+package ru.ugochs.erm.view;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.router.*;
+import ru.ugochs.erm.entity.Street;
+import ru.ugochs.erm.service.crud.Db;
+import ru.ugochs.erm.service.crud.GetAll;
+import ru.ugochs.erm.view.component.*;
+import ru.ugochs.erm.view.form.add.AddStreetForm;
+import ru.ugochs.erm.view.form.edit.EditStreetForm;
+
+@PageTitle("Улицы")
+@Route("streets")
+public class StreetView extends FullSizedVerticalLayout implements BeforeEnterObserver {
+    private final Db db;
+    private final Grid<Street> streets;
+
+    public StreetView(Db db) {
+        this.db = db;
+        this.streets = new StandardGrid<>(
+            new GridColumns<>(
+                new GridColumn<>(
+                    Street::getName,
+                    "Улица"
+                ),
+                new GridColumn<>(
+                    street -> street.getDistrict().getName(),
+                    "Район"
+                )
+            ),
+            clickEvent -> new NavigationByEntity(
+                clickEvent.getItem(),
+                EditStreetForm.class
+            ).perform()
+        );
+        this.add(
+            new StandardHorizontalLayout(
+                new H2("Заявители"),
+                new Button(
+                    "Добавить",
+                    event -> new Navigation(
+                        AddStreetForm.class
+                    ).perform()
+                )
+            ),
+            this.streets
+        );
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        this.streets.setItems(
+            new GetAll<>(
+                this.db,
+                Street.class
+            ).perform());
+    }
+}
