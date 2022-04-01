cf06205e0e9c9ccd0c939f3ac4133b89d9b0bf1d
sl4r1k <sl4r1k@gmail.com>
2022-04-01 19:10:25 +0300

    добавить форму сохранения улицы

---
+++ src/main/java/ru/ugochs/erm/view/form/add/AddStreetForm.java
@@ -0,0 +1,30 @@
+package ru.ugochs.erm.view.form.add;
+
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.Route;
+import ru.ugochs.erm.entity.Street;
+import ru.ugochs.erm.service.crud.*;
+import ru.ugochs.erm.view.component.AddButton;
+import ru.ugochs.erm.view.component.CancelButton;
+import ru.ugochs.erm.view.form.StreetForm;
+
+@Route("indexes/add")
+public class AddStreetForm extends StreetForm {
+    public AddStreetForm(Db db) {
+        super(db);
+        this.binder.setBean(new Street());
+        this.district.setItems(
+            new GetAllDistrictNames(this.db).perform()
+        );
+        this.add(
+            new HorizontalLayout(
+                new AddButton<>(
+                    street -> new AddStreet(street, this.db),
+                    this.binder,
+                    StreetView.class
+                ),
+                new CancelButton(StreetView.class)
+            )
+        );
+    }
+}
