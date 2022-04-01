66f2832b873ef3d69313b86a6faefa7e493254fd
sl4r1k <sl4r1k@gmail.com>
2022-04-01 19:29:45 +0300

    добавить форму редактирования улицы

---
+++ src/main/java/ru/ugochs/erm/view/form/edit/EditStreetForm.java
@@ -0,0 +1,53 @@
+package ru.ugochs.erm.view.form.edit;
+
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.*;
+import ru.ugochs.erm.entity.Street;
+import ru.ugochs.erm.service.crud.*;
+import ru.ugochs.erm.view.IdFromRoute;
+import ru.ugochs.erm.view.component.*;
+import ru.ugochs.erm.view.form.StreetForm;
+
+@Route("streets/:id/edit")
+public class EditStreetForm extends StreetForm implements BeforeEnterObserver {
+    public EditStreetForm(Db db) {
+        super(db);
+        this.add(
+            new HorizontalLayout(
+                new EditButton<>(
+                    street -> new EditStreet(street, this.db),
+                    this.binder,
+                    StreetView.class
+                ),
+                new RemoveButton<>(
+                    street -> new Remove<>(street, this.db),
+                    this.binder,
+                    StreetView.class
+                ),
+                new CancelButton(StreetView.class)
+            )
+        );
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        new GetById<>(
+            new IdFromRoute(event.getRouteParameters()).value(),
+            this.db,
+            Street.class
+        ).perform().ifPresentOrElse(
+            street -> {
+                this.binder.setBean(street);
+                String district = this.district.getValue();
+                this.district.setItems(
+                    new GetAllDistrictNames(this.db).perform()
+                );
+                this.district.setValue(district);
+            },
+            () -> event.rerouteToError(
+                NotFoundException.class,
+                "Нет такого индекса"
+            )
+        );
+    }
+}
