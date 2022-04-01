ec5cf9bb012b79012734ddb083b9fc16f66d9965
sl4r1k <sl4r1k@gmail.com>
2022-04-01 23:08:56 +0300

    добавить конструкторы в модель района

--- src/main/java/ru/ugochs/erm/entity/District.java
+++ src/main/java/ru/ugochs/erm/entity/District.java
@@ -5,10 +5,15 @@ import javax.persistence.*;
 
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 @Data
 @Table(name = "districts")
 @Entity
 public class District extends AbstractEntity {
     @Column(name = "name", nullable = false, unique = true)
     private String name;
+
+    public District(String name) {
+        this.name = name;
+    }
 }
