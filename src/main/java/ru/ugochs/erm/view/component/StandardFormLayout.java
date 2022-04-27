38450a379ca50440f7198b88440f6e395aba23b8
sl4r1k <sl4r1k@gmail.com>
2022-04-27 17:02:39 +0300

    фикс

--- src/main/java/ru/ugochs/erm/view/component/StandardFormLayout.java
+++ src/main/java/ru/ugochs/erm/view/component/StandardFormLayout.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 public class StandardFormLayout extends FormLayout {
     public StandardFormLayout(Component... components) {
         super(components);
-        this.getStyle().set("margin", "auto");
+        this.getStyle().set("margin", "0% 35% 0% 35%");
+        this.setResponsiveSteps(new ResponsiveStep("0", 1, ResponsiveStep.LabelsPosition.TOP));
     }
 }
