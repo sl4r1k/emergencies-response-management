cbd8400485339445bb5d76c555d738f423eaaee9
sl4r1k <sl4r1k@gmail.com>
2022-04-27 16:51:32 +0300

    фикс

--- src/main/java/ru/ugochs/erm/entity/Employee.java
+++ src/main/java/ru/ugochs/erm/entity/Employee.java
@@ -1,6 +1,7 @@
 package ru.ugochs.erm.entity;
 
 import lombok.*;
+import org.hibernate.annotations.Formula;
 import javax.persistence.*;
 
 @ToString(callSuper = true)
@@ -19,28 +20,18 @@ public class Employee extends AbstractEntity {
     @Column(name = "middle_name", nullable = false)
     private String middleName;
 
+    @Setter(AccessLevel.NONE)
+    @Formula("CONCAT(last_name, ' ', first_name, ' ', middle_name)")
+    private String fullName;
+
+    @Setter(AccessLevel.NONE)
+    @Formula("CONCAT(last_name, ' ', SUBSTRING(first_name, 0, 2), '. ', SUBSTRING(middle_name, 0, 2), '.')")
+    private String initials;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false)
     private Role role;
 
     @Column(name = "password", nullable = false)
     private String password;
-
-    public String fullName() {
-        return String.format(
-            "%s %s %s",
-            this.lastName,
-            this.firstName,
-            this.middleName
-        );
-    }
-
-    public String initials() {
-        return String.format(
-            "%s %s. %s.",
-            this.lastName,
-            this.firstName.substring(0, 1),
-            this.lastName.substring(0, 1)
-        );
-    }
 }
