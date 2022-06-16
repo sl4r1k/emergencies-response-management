8b276e5df7e0f26c4c1d3e13178b8c7a41b08bef
sl4r1k <sl4r1k@gmail.com>
2022-06-17 00:30:34 +0300

    добавить русскую локаль

--- src/main/java/ru/ugochs/erm/view/component/RussianDatePicker.java
+++ src/main/java/ru/ugochs/erm/view/component/RussianDatePicker.java
@@ -2,6 +2,7 @@ package ru.ugochs.erm.view.component;
 
 import com.vaadin.flow.component.datepicker.DatePicker;
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class RussianDatePicker extends DatePicker {
     public RussianDatePicker(String label) {
@@ -11,5 +12,6 @@ public class RussianDatePicker extends DatePicker {
     public RussianDatePicker(String label, LocalDate value) {
         super(label, value);
         this.setI18n(new DatePickerRussianI18n());
+        this.setLocale(new Locale("ru"));
     }
 }
--- src/main/java/ru/ugochs/erm/view/component/RussianDateTimePicker.java
+++ src/main/java/ru/ugochs/erm/view/component/RussianDateTimePicker.java
@@ -1,11 +1,12 @@
 package ru.ugochs.erm.view.component;
 
 import com.vaadin.flow.component.datetimepicker.DateTimePicker;
-import ru.ugochs.erm.view.component.DatePickerRussianI18n;
+import java.util.Locale;
 
 public class RussianDateTimePicker extends DateTimePicker {
     public RussianDateTimePicker(String label) {
         super(label);
         this.setDatePickerI18n(new DatePickerRussianI18n());
+        this.setLocale(new Locale("ru"));
     }
 }
